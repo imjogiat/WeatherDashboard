0742e1c7321c22492b2ec9a8315c231f77fba23c
Ismail <imjogiat@gmail.com>
2025-05-15 16:15:42 -0600

    added a graph and dynamic days

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+
+def get_data(d):
+    dates = pd.date_range("2024-01-01","2024-01-30")
+
+#temperatures are temporarily a defined list- will obtain them from the API later
+    temperatures = range(10,40)
+    temperatures = [(d * i) for i in temperatures]
+
+    return dates, temperatures
+
 
 st.title("Weather Forecast")
 place = st.text_input("Place: ")
@@ -8,3 +21,13 @@ days = st.slider("Forecasted Days: ", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",options=["Temperature", "Sky condition"])
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+
+#first create a plotly figure using plotly then use it as parameter for st.plotlychart()
+#x is horiz data, y is corresponding value data, labels are axis labels
+#x and y take array type (list, tuple, DF column/series, ...)
+
+dates, temperatures = get_data(days)
+
+figure = px.line(x=dates, y=temperatures, labels={"x":"Date", "y":"Temperature (C)"})
+st.plotly_chart(figure)
